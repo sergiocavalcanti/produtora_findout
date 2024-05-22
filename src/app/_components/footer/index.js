b9f663539fc9a80d841c2fd13b3f4b63059ed9e7
Sergio Cavalcanti <sergiocavalcanti.ti@gmail.com>
2024-05-22 19:46:58 -0300

    Quase final push

--- src/app/_components/footer/index.js
+++ src/app/_components/footer/index.js
@@ -6,7 +6,8 @@ const Footer = () => {
   return (
     <div className={style.footer}>
         <ul>
-            <p>Contatos: instagram/</p>
+            <p>Contatos</p>
+            <img src="https://cdn-icons-png.flaticon.com/512/87/87390.png" Link="https://www.instagram.com/produtorafindout/"/>
         </ul>
     </div>
   )
